dfcb0ba14a3f97c3a7bf20aecfab40fb249b7dfc
BrianMendez <bm930731@gmail.com>
2025-10-15 23:28:02 -0600

    ¡primer push del proyecto de desarrollo web!

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173
+  },
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    host: true // ¡IMPORTANTE para Render!
   }
 })
